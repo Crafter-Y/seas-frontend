fe0360e199be522dd7dd7170fa1a735546cb089f
Crafter_Y <mrcrafter.yt1@gmail.com>
2023-10-27 14:56:08 +0200

    adjustet serve.js to support multiserver subdomains (possibly)

--- serve.js
+++ serve.js
@@ -2,12 +2,33 @@ const handler = require("serve-handler");
 const http = require("http");
 
 const server = http.createServer((request, response) => {
+  if (request.url == "/assets/config.json" && process.env.REWRITE_CONFIG) {
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    response.end(
+      JSON.stringify({
+        serverId: request.headers.serverid,
+      })
+    );
+    return;
+  } else if (process.env.SERVER_ID && request.url == "/assets/config.json") {
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    response.end(
+      JSON.stringify({
+        serverId: process.env.SERVER_ID,
+      })
+    );
+    return;
+  }
+
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/vercel/serve-handler#options
   return handler(request, response, {
     public: "./dist",
     rewrites: [
-        //{ source: "/assets/config.json", destination: "/assets/config.json" },
         { source: "/**/*", destination: "/index.html" },
       ]
   });
@@ -17,4 +38,9 @@ const PORT = process.env.PORT || 3333;
 
 server.listen(PORT, () => {
   console.log(`Running at ::${PORT}`);
+  if (process.env.REWRITE_CONFIG) {
+    console.log("Rewriting serverId to header")
+  } else if (process.env.SERVER_ID) {
+    console.log("Rewriting serverId to env: '" + process.env.SERVER_ID + "'")
+  }
 });
