e281dad9ed2ce14102078ff9afdf809762fcce1f
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-09-21 18:05:29 +0200

    bump version before build

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "SEAS - Kirchengemeinden",
     slug: "csa-frontend",
     scheme: "seas-kirchengemeinde",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "default",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
